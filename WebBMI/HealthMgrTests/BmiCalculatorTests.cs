301cdcce8a45572c49b6b43223f4b2dc54d06422
twdeveloper <twdeveloper@outlook.com>
2019-10-12 20:50:06 +0800

    add unit test

---
+++ WebBMI/HealthMgrTests/BmiCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthMgr.Tests
+{
+    [TestClass()]
+    public class BmiCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.Height = 170;
+            bmi.Weight = 70;
+
+            var result = bmi.Calculate();
+
+            Assert.AreEqual("24.22", result.ToString("00.00"));
+        }
+    }
+}
